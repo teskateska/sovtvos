f40ea64430f39913dd99cb7187aaa07de52ef756
Harris Ebdon <101606043@student.swin.edu.au>
2024-08-31 16:39:25 +1000

    Add political message

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hello World</h1>
+  <div class="mx-auto w-full h-screen bg-black">
+    <div
+      class="mx-auto w-1/2 h-screen my-10 flex flex-col items-center justify-center text-center space-y-12"
+    >
+      <h1 class="text-white text-5xl sm:text-7xl font-neue-metana-bold">
+        Kamala Harris for President.
+      </h1>
+      <div class="flex flex-col space-y-2">
+        <p class="text-white font-neue-metana-normal">
+          Big things coming here soon. Watch this space...
+        </p>
+        <p class="text-white font-neue-metana-normal"></p>
+      </div>
+    </div>
+  </div>
 </template>
